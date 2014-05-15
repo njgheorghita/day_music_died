db01f1c0ad0665ee803ed757feb54c944e0c820b
Kyle <kyle@kyle-B709.(none)>
2014-05-15 10:28:33 -0600

    added image tagging call

--- alchemyapi.rb
+++ alchemyapi.rb
@@ -80,8 +80,11 @@ class AlchemyAPI
 	@@ENDPOINTS['combined'] = {}
 	@@ENDPOINTS['combined']['url'] = '/url/URLGetCombinedData'
 	@@ENDPOINTS['combined']['text'] = '/text/TextGetCombinedData'
-	@@ENDPOINTS['image'] = {}
-	@@ENDPOINTS['image']['url'] = '/url/URLGetImage'
+	@@ENDPOINTS['image_extract'] = {}
+	@@ENDPOINTS['image_extract']['url'] = '/url/URLGetImage'
+	@@ENDPOINTS['image_tag'] = {}
+	@@ENDPOINTS['image_tag']['url'] = '/url/URLGetRankedImageKeywords'
+	@@ENDPOINTS['image_tag']['image'] = '/image/ImageGetRankedImageKeywords'
 		
 	@@BASE_URL = 'http://access.alchemyapi.com/calls'
 	
@@ -567,11 +570,11 @@ class AlchemyAPI
 	end
 
 
-	# Extract image from  a URL.
+	# Extract image from a URL.
 	# 
 	# INPUT:
 	# flavor -> which version of the call, i.e.  url.
-	# data -> the data to analyze, either the the url, or html code.
+	# data -> the data to analyze, i.e. the url.
 	# options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
 	#
 	# Available Options:
@@ -580,19 +583,49 @@ class AlchemyAPI
 	# OUTPUT:
 	# The response, already converted from JSON to a Ruby object. 
 	#
-	def image(flavor, data, options = {})
-		unless @@ENDPOINTS['image'].key?(flavor)
+	def image_extract(flavor, data, options = {})
+		unless @@ENDPOINTS['image_extract'].key?(flavor)
 			return { 'status'=>'ERROR', 'statusInfo'=>'Image for ' + flavor + ' not available' }
 		end
 
 		#Add the URL encoded data to the options and analyze
 		options[flavor] = data
-		return analyze(@@ENDPOINTS['image'][flavor], options)
+		return analyze(@@ENDPOINTS['image_extract'][flavor], options)
+	end
+
+
+	# Tag image from a URL or raw image data.
+	# For an overview, please refer to: http://www.alchemyapi.com/products/features/image-tagging/
+	# For the docs, please refer to: http://www.alchemyapi.com/api/image-tagging/
+	# 
+	# INPUT:
+	# flavor -> which version of the call, i.e.  url or image.
+	# data -> the data to analyze, the url
+	# options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	#
+	# Available Options:
+	# extractMode -> trust-metadata: less CPU-intensive and less accurate, always-infer: more CPU-intensive and more accurate
+	# (image flavor only)
+	# imagePostMode -> how you will post the image
+	#  		raw: pass an unencoded image file using POST
+	#
+	# OUTPUT:
+	# The response, already converted from JSON to a Ruby object. 
+	#
+	def image_tag(flavor, data, options = {}, image = '')
+		unless @@ENDPOINTS['image_tag'].key?(flavor)
+			return { 'status'=>'ERROR', 'statusInfo'=>'Image tagging for ' + flavor + ' not available' }
+		end
+
+		#Add the URL encoded data to the options and analyze
+		unless data.empty?
+			options[flavor] = data
+		end
+		return analyze_image(@@ENDPOINTS['image_tag'][flavor], options, image)
 	end
 
 
 	private
-
 
 	# HTTP Request wrapper that is called by the endpoint functions. This function is not intended to be called through an external interface. 
 	# It makes the call, then converts the returned JSON string into a Ruby object. 
@@ -611,9 +644,62 @@ class AlchemyAPI
 		#Add the API key and set the output mode to JSON
 		options['apikey'] = @apiKey
 		options['outputMode'] = 'json'
-		
+
+		uri = URI.parse(url)
+		request = Net::HTTP::Post.new(uri.request_uri)
+		request.set_form_data(options)
+
+		# disable gzip encoding which blows up in Zlib due to Ruby 2.0 bug
+		# otherwise you'll get Zlib::BufError: buffer error
+		request['Accept-Encoding'] = 'identity'
+
 		#Fire off the HTTP request
-		res = Net::HTTP::post_form(URI.parse(url), options)
+		res = Net::HTTP.start(uri.host, uri.port) do |http|
+      		http.request(request)
+    	end
+
+		#parse and return the response
+		return JSON.parse(res.body)
+	end
+
+	# HTTP Request wrapper that is called by the endpoint functions. This function is not intended to be called through an external interface. 
+	# It makes the call, then converts the returned JSON string into a Ruby object. 
+	#
+	# INPUT:
+	# url -> the full URI encoded url
+	# body -> the raw binary image data
+	# 
+	# OUTPUT:
+	# The response, already converted from JSON to a Ruby object. 
+	# 
+	def analyze_image(url, options, body)
+
+		#Insert the base URL
+		url = @@BASE_URL + url
+
+		#Add the API key and set the output mode to JSON
+		options['apikey'] = @apiKey
+		options['outputMode'] = 'json'
+
+		url += '?'
+		options.each { |h,v|
+			url += h + '=' + v + '&'
+		}
+
+		#Parse URL
+		uri = URI.parse(url)
+
+		request = Net::HTTP::Post.new(uri.request_uri)
+		request.body = body.to_s
+
+		# disable gzip encoding which blows up in Zlib due to Ruby 2.0 bug
+		# otherwise you'll get Zlib::BufError: buffer error
+		request['Accept-Encoding'] = 'identity'
+
+		#Fire off the HTTP request
+		res = Net::HTTP.start(uri.host, uri.port) do |http|
+			http.request(request)
+		end
 		
 		#parse and return the response
 		return JSON.parse(res.body)
